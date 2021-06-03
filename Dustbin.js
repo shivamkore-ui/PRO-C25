aea9e2c8a1a13d040437b0ee8bf8c875fb376747
shivamkore-ui <77399546+shivamkore-ui@users.noreply.github.com>
2021-06-03 18:34:17 +0530

    Update Dustbin.js

--- Dustbin.js
+++ Dustbin.js
@@ -1,15 +1,12 @@
 class Dustbin {
     constructor (x,y,width,height){
-     
-        this.body = Bodies.rectangle(x,y,width,height)
-        World.add(world, this.body)
-        this.width = width;
+             this.width = width;
         this.height = height;
         this.image = loadImage("dustbingreen.png")
 
     }
 
     display(){
-     image(this.image, this.body.position.x, this.body.position.y, this.width, this.height)
+     image(this.image, 475,585,100, 100)
     }
 }
